d8ca49c77a5ddd0e6433d20df6c38f9ed2e53c9a
ImuraHaruki <3haricca@gmail.com>
2025-12-28 16:09:21 +0900

    コンパイルファイルを追加

---
+++ compile.bat
@@ -0,0 +1,11 @@
+@echo off
+chcp 65001 > nul
+cd /d "%~dp0"
+echo コンパイルを開始します...
+javac -d out -sourcepath . -cp "lib\*" jp/ac/kyoto_u/kueps/STube/STube.java jp/ac/kyoto_u/kueps/naruse_utils/MiscUtils.java jp/ac/kyoto_u/kueps/naruse_utils/NFontChooser.java jp/ac/kyoto_u/kueps/naruse_utils/PSExporter.java gnu/io/CommPort.java gnu/io/CommPortIdentifier.java gnu/io/NoSuchPortException.java gnu/io/PortInUseException.java gnu/io/SerialPort.java gnu/io/UnsupportedCommOperationException.java
+if %errorlevel% equ 0 (
+    echo コンパイルが完了しました！
+) else (
+    echo コンパイルにエラーが発生しました。
+)
+pause
